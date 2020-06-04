4a1cbb8b3c2e54aaa915a4b5acb7161c56cc3502
Alex Hettle <51217513+AlexHettle@users.noreply.github.com>
2020-06-04 16:07:06 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <limits>
-long long combinations(int n,int r){
+//calculates the combinations C(n,r)
+//Long long is used here because although the end results of C(n,r)
+//are rather small, the process to create the end result can use
+//extremely large numbers.
+int combinations(int n,int r){
     long long sumn=1;
     long long sumr=1;
     long long sumnr=1;
+    //all for loops used to create n!,r!, and (n-r)!
     for(int i=2;i<=n;i++){sumn=sumn*i;}
     for(int i=2;i<=r;i++){sumr=sumr*i;}
     for(int i=2;i<=(n-r);i++){sumnr=sumnr*i;}
+    //returns the C(n,r)...(n!/(r!(n-r)!))
     return sumn/(sumr*sumnr);
 }
+//Prints out all C(n,r) from zero to n
 void allcombinations(int n){
     for(int i=0;i<=n;i++){
         std::cout<<"C("<<n<<","<<i<<") = "<<combinations(n,i)<<"\n";
     }
 }
+//Takes in user input n for the program to find C(n,0),C(n,1),...,C(n,n)
 void enter_n(){
     int n;
     std::cout<<"please enter a valid number for n that is less than or equal to 20\n";
